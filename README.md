c4af04981586975cd8e11d46eab140b97444cd71
Bogdan Sandu <bogdanelsandu2011@gmail.com>
2026-04-14 13:58:55 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SafeJS — A runtime designed for safety
+SajeJS is a javascript runtime based on Node.js, designed for safe execution of untrusted code/scripts.
---
+++ src/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import ivm from 'isolated-vm';
+
+const isolate = new ivm.Isolate({ memoryLimit: 512 });
+const context = await isolate.createContext();
+
+const jail = context.global;
+await jail.set('global', jail.derefInto());
+
+await jail.set(
+    '__internal_consoleLog',
+    new ivm.Reference((level, msg) => {
+        if (level === 0) {
+            process.stdout.write(`[INFO]  ${msg}`);
+        } else if (level === 1) {
+            process.stderr.write(`[ERROR] ${msg}`);
+        }
+    })
+);
+
+await context.eval(`
+    globalThis.console = {
+        info: (msg) => __internal_consoleLog(0, msg),
+        error: (msg) => __internal_consoleLog(1, msg)
+    }
+`);
+
+import * as fs from 'fs';
+import { loadEnvFile } from 'process';
+
+const code = fs.readFileSync(process.argv[2], "utf-16le");
+
+await context.eval(code);
